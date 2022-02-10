3130006d9db3097adeac4f86f4bcfcbeddf7ce77
satishsampate <sesampate@gmail.com>
2022-02-10 22:02:42 +0530

    commiting this file changes.

--- src/main/java/MyMainApplication.java
+++ src/main/java/MyMainApplication.java
@@ -1,5 +1,7 @@
 public class MyMainApplication {
     public static void main(String[] args) {
+
+        System.out.println("This is my maven project in git.");
         System.out.println("This is my maven project in git.");
     }
 }
